fabf89f7d2e88bd866ec6b90c696d90e789ffa09
iameunbin <eunbin_dev@naver.com>
2021-02-17 15:22:00 +0900

    google image crawling

---
+++ google_crawling/selenium/google.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+# chrome browser로 들어가기
+driver = webdriver.Chrome()
+# 이미지 검색 창으로 들어가기
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+elem = driver.find_element_by_name("q")
+# search image
+elem.send_keys("된장찌개")
+# enter
+elem.send_keys(Keys.RETURN)
+
+# scroll down
+SCROLL_PAUSE_TIME = 1
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+
+# image select, click
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        # big image
+        imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+        # image download
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
